df067cafafb0f9fa5aa844421f92e000861f6486
isaacgutierrez <jose19.26@>
2018-08-27 23:19:24 -0500

    images

--- fontmanager/src/main/java/com/manager/font/fontmanager/ext/FontAwesomeTextView.java
+++ fontmanager/src/main/java/com/manager/font/fontmanager/ext/FontAwesomeTextView.java
@@ -1,27 +1,27 @@
 package com.manager.font.fontmanager.ext;
 
-
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.widget.Button;
+import android.widget.TextView;
 
 import com.manager.font.fontmanager.FontIconIGB;
 import com.manager.font.fontmanager.R;
 
-public class FontAwesomeButton extends Button {
+public class FontAwesomeTextView extends TextView {
 
     public String fontawesome;
 
-    public FontAwesomeButton(Context context, AttributeSet attrs) {
+    public FontAwesomeTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.FontAwesomeButton, 0, 0);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.FontAwesomeTextView, 0, 0);
 
-        String str = a.getString(R.styleable.FontAwesomeButton_fontawesome);
-        FontIconIGB.Companion.setTextFontAwesome_Button(this);
+        //String str = a.getString(R.styleable.FontAwesomeButton_fontawesome);
+        FontIconIGB.Companion.setTextFontAwesome_Text_View(this);
 
     }
+
 
     public String getFontawesome() {
         return fontawesome;
