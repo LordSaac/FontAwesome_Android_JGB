da85c9b14ca8160789ee98d512d49e040f584d6c
jgutierrez <jose19.26@hotmail.com>
2019-02-01 15:44:21 -0500

    Released February 1, 2019

--- app/src/main/java/com/manager/font/fontlibreryigb/MainActivity.kt
+++ app/src/main/java/com/manager/font/fontlibreryigb/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         // for more icon here: https://fontawesome.com/icons?d=gallery&m=free
         tv_icon_two.setTextFontAwesome("\uF2B9")
         // load icon using name fonts
-        tv_icon_three.setTextFontAwesome("cannabis")
+        tv_icon_three.setTextFontAwesome("biohazard")
         // load icon
         switch1.setTextFontAwesome("snowflake")
         // load icon
